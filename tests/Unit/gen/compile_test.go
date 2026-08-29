8341689dda8c74ff8077028266e9306b7b70816e
HYZIS <admin@hyz.is>
2026-08-29 15:22:38 -0300

    Compile generators against the released security floor
    Signed-off-by: HYZIS <admin@hyz.is>

--- tests/Unit/gen/compile_test.go
+++ tests/Unit/gen/compile_test.go
@@ -28,8 +28,8 @@ import (
 // The cost is that these three lines go stale the day the skeleton moves, and
 // that is what TestThePinnedTagsMatchTheSkeleton answers.
 var published = map[string]string{
-	"github.com/arandu-io/framework": "v0.40.0",
-	"github.com/arandu-io/hesape":    "v0.19.0",
+	"github.com/arandu-io/framework": "v0.41.0",
+	"github.com/arandu-io/hesape":    "v0.19.1",
 	"github.com/arandu-io/kyse":      "v0.12.1",
 }
 
